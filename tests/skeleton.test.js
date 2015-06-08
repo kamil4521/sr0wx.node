ecb12ecfa393f22684c4fe004b8ee554a901a89a
Giskard Solutions <kamil4521@gmail.com>
2015-06-08 18:53:04 +0200

    Added example test

---
+++ tests/skeleton.test.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+
+describe('Skeleton tests', function(){
+
+    describe('Example skeleton test', function(){
+        before(function(done){
+            done();
+        });
+
+        after(function(done){
+            done();
+        });
+
+        it('Should ok', function(done){
+            expect(true).is.true;
+            done();
+        });
+    });
+});
